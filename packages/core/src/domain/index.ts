423cec65368e513548219b12c6fe2250d74c956c
Andrey Krasovsky <an.krasowsckij@gmail.com>
2021-02-21 22:53:23 +0300

    feat: added OC create machine

--- packages/core/src/domain/index.ts
+++ packages/core/src/domain/index.ts
@@ -1,23 +1,55 @@
 import {check} from '../utils/check';
 
 interface MachineEvent {
-  name: string;
+  getName: () => string;
+}
+
+interface State {
+  getName: () => string;
 }
 
 interface Transition {
-  from: string;
-  to: string;
+  stateFrom: State;
+  stateTo: State;
   event: MachineEvent;
 }
 
 interface MachineNode {
-  name: string;
+  getName: () => string;
+  getEvents: () => MachineEvent[];
+  send: (event: MachineEvent) => void;
 }
 
 interface MachineTools {
-  event: (newEvent: MachineEvent) => MachineEvent;
-  transition: (newTransition: Transition) => Transition;
+  event: (eventName: string) => MachineEvent;
+  state: (stateName: string) => State;
+  transition: (stateFrom: State, stateTo: State, event: MachineEvent) => void;
+  machine: (intiState: State) => MachineNode;
 }
+
+const createEvent = (eventName: string) => {
+  const getName = () => {
+    return eventName;
+  };
+
+  return {getName};
+};
+
+const createState = (stateName: string) => {
+  const getName = () => {
+    return stateName;
+  };
+
+  return {getName};
+};
+
+const createTransition = (stateFrom: State, stateTo: State, event: MachineEvent): Transition => {
+  return {
+    stateFrom,
+    stateTo,
+    event,
+  };
+};
 
 const createMachine = (
   machineName: string,
@@ -26,32 +58,108 @@ const createMachine = (
   check([!machineName, `Machine name is required.`]);
 
   const events: MachineEvent[] = [];
+  const states: State[] = [];
   const transitions: Transition[] = [];
 
-  const event = (newEvent: MachineEvent): MachineEvent => {
+  const event = (eventName: string): MachineEvent => {
     check([
-      !events.find((event) => event.name === newEvent.name),
-      `Machine "${machineName}" must have only uniq events. Event "${newEvent.name}" is duplicated.`,
+      !events.find((event) => event.getName() === eventName),
+      `Machine "${machineName}" must have only uniq events. Event "${eventName}" is duplicated.`,
     ]);
 
+    const newEvent = createEvent(eventName);
     events.push(newEvent);
     return newEvent;
   };
 
-  const transition = (newTransition: Transition): Transition => {
+  const state = (stateName: string): State => {
     check([
-      !transitions
-        .filter((transition) => transition.from === newTransition.from)
-        .filter((transition) => transition.event.name === newTransition.event.name).length,
-      `Machine "${machineName}" state "${newTransition.from}" must have single transition with event "${newTransition.event.name}".`,
+      !states.find((state) => state.getName() === stateName),
+      `Machine "${machineName}" must have only uniq states. State "${stateName}" is duplicated.`,
     ]);
 
+    const newState = createState(stateName);
+    states.push(newState);
+    return newState;
+  };
+
+  const transition = (stateFrom: State, stateTo: State, transitionEvent: MachineEvent) => {
+    check(
+      [
+        !states.find((state) => state === stateFrom),
+        `State "stateFrom" of transition "${machineName} not created with machine "state" function".`,
+      ],
+      [
+        !states.find((state) => state === stateTo),
+        `State "stateTo" of transition "${machineName} not created with machine "state" function".`,
+      ],
+      [
+        !events.find((event) => event === transitionEvent),
+        `Event of transition "${machineName} not created with machine "event" function".`,
+      ],
+      [
+        !transitions
+          .filter((transition) => transition.stateFrom.getName() === stateFrom.getName())
+          .filter((transition) => transition.event.getName() === transitionEvent.getName()).length,
+        `Machine "${machineName}" state "${stateFrom.getName()}"
+      must have single transition with event "${transitionEvent.getName()}".`,
+      ],
+    );
+
+    const newTransition = createTransition(stateFrom, stateTo, transitionEvent);
     transitions.push(newTransition);
-    return newTransition;
+  };
+
+  const machine = (intiState: State): MachineNode => {
+    check([
+      !states.find((state) => state === intiState),
+      `Initial state for machine "${machineName} not created with machine "state" function".`,
+    ]);
+
+    let currentState = intiState;
+
+    const send = (targetEvent: MachineEvent) => {
+      if (!events.find((event) => event === targetEvent)) {
+        return;
+      }
+
+      const targetTransitions = transitions
+        .filter((transition) => transition.event === targetEvent)
+        .filter((transition) => transition.stateFrom === currentState);
+
+      if (targetTransitions.length === 0) {
+        return;
+      }
+
+      check([
+        targetTransitions.length > 1,
+        `Unexpected transition in machine "${machineName}". By some reason state "${currentState.getName()}"
+        has more then one attached transition with event "${targetEvent.getName()}".`,
+      ]);
+
+      const targetTransition = targetTransitions[0];
+      currentState = targetTransition.stateTo;
+    };
+
+    const getName = () => {
+      return machineName;
+    };
+
+    const getEvents = () => {
+      return [...events];
+    };
+
+    return {
+      getName,
+      getEvents,
+      send,
+    };
   };
 
   return stateConstructor({
     event,
+    state,
     transition,
+    machine,
   });
 };
