8798e9df45a8cfa9e2dd8f164f508b494cb389a3
Andrey Krasovsky <an.krasowsckij@gmail.com>
2021-02-22 19:14:29 +0300

    feat: prototype type frndly scheme creator

--- packages/core/src/domain/index.ts
+++ packages/core/src/domain/index.ts
@@ -1,17 +1,77 @@
-type NotNullableString<T extends string> = T extends '' ? never : T;
+interface MachineEvent {}
 
-const createEvent = <T extends string>(name: NotNullableString<T>) => {
-  const getName = (): T => {
-    return name;
-  };
-
-  return {getName};
+const createEvent = (): MachineEvent => {
+  return {};
 };
 
-const createState = <T extends string>(name: NotNullableString<T>) => {
-  const getName = (): T => {
-    return name;
-  };
+interface Store {}
 
-  return {getName};
+type Action<T extends Store> = (store: T) => T;
+
+interface StoreContainer<T extends Store> {
+  store: T;
+  createAction: (action: Action<T>) => Action<T>;
+}
+
+const createStore = <T extends Store>(store: T): StoreContainer<T> => {
+  return {
+    store,
+    createAction: (action) => action,
+  };
 };
+
+interface Transition<T extends Store> {
+  nodeFrom: MachineNode<T>;
+  nodeTo: MachineNode<T>;
+  effects: Action<T>[];
+}
+
+interface MachineNode<T extends Store> {
+  leave: Action<T>[];
+  entry: Action<T>[];
+}
+
+interface Machine<T extends Store> extends MachineNode<T> {
+  type: 'machine';
+  getStore: () => T;
+  send: (event: MachineEvent) => void;
+}
+
+interface State<T extends Store> extends MachineNode<T> {
+  type: 'state';
+}
+
+const createState = <T extends Store>(node: MachineNode<T>): MachineNode<T> => {
+  return node;
+};
+
+interface Scheme<T extends Store> {
+  init: State<T>;
+  scheme: T;
+  events: MachineEvent[];
+  states: State<T>[];
+  transitions: Transition<T>[];
+}
+
+const createMachine = <T extends Store>(scheme: Scheme<T>) => {};
+
+// Textures
+
+const {store, createAction} = createStore({
+  count: 0,
+});
+
+const increment = createAction((state) => ({
+  count: state.count + 1,
+}));
+
+const decrement = createAction((state) => ({
+  count: state.count - 1,
+}));
+
+const state1 = createState({
+  entry: [increment],
+  leave: [decrement],
+});
+
+// createMachine({})
