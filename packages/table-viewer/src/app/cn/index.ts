4d3f9e303009af272f6ec04e590cf892c5d08fe3
Andrey Krasovsky <an.krasowsckij@gmail.com>
2021-03-18 23:44:24 +0300

    feat: add package linking

--- packages/table-viewer/src/app/cn/index.ts
+++ packages/table-viewer/src/app/cn/index.ts
@@ -3,3 +3,5 @@ type CnItem = string | boolean | undefined;
 export const cn = (...classnames: CnItem[]) => {
   return classnames.filter((classname) => !!classname && typeof classname === 'string').join(' ');
 };
+
+// trdt
